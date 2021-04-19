6b1fd6ea8d17240037219533426cd1c4745040ad
Ben Rasmussen <rasmussen.ben32@gmail.com>
2021-04-19 14:53:30 -0500

    completed readme and tasks 1-2

--- index.js
+++ index.js
@@ -28,13 +28,13 @@ console.log(processFirstItem(['foo','bar'],function(str){return str+str}));
   Study the code for counter1 and counter2, then answer the questions below.
   
   1. What is the difference between counter1 and counter2?
-  
+    count is function scoped in counter1 and globally scoped in counter2.
   2. Which of the two uses a closure? How can you tell?
-  
+    counter2 utilizes a closure at count++ is reaching outside of the counter2 function to determine the value of count.
   3. In what scenario would the counter1 code be preferable? In what scenario would 
-     counter2 be better?  
+     counter2 be better?
+     ???
 */
-//test2
 // counter1 code
 function counterMaker() {
   let count = 0;
@@ -62,8 +62,8 @@ Use the inning function below to do the following:
 NOTE: This will be a callback function for the tasks below
 */
 
-function inning(/*Code Here*/){
-    /*Code Here*/
+function inning(){
+    return Math.ceil(Math.random() * 2);
 }
 
 
